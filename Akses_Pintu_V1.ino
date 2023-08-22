8138c7a9804c001c93aa7a16bec2cd4dddfc2b21
mitsalgh <121527453+mitsalgh@users.noreply.github.com>
2023-08-23 03:31:24 +0700

    Update Akses_Pintu_V1.ino

--- Akses_Pintu_V1.ino
+++ Akses_Pintu_V1.ino
@@ -20,64 +20,63 @@
 #define pinRelaySolenoid 2
 #define pinSensorNoTouch A2
 
-String statePintu="closed";
-String dataSTB="";
-String modeProgram="";
+String statePintu = "closed";
+String dataSTB = "";
+String modeProgram = "";
 
 
 void setup() {
- Serial.begin(9600);
- pinMode(pinLimitSwitch, INPUT_PULLUP);
- pinMode(pinSensorNoTouch,INPUT_PULLUP);
- pinMode(pinRelaySolenoid,OUTPUT);
- statePintu="closed";
- modeProgram="testing"; 
+  Serial.begin(9600);
+  pinMode(pinLimitSwitch, INPUT_PULLUP);
+  pinMode(pinSensorNoTouch, INPUT_PULLUP);
+  pinMode(pinRelaySolenoid, OUTPUT);
+  statePintu = "closed";
+  modeProgram = "testing";
 }
 
-void loop() 
+void loop()
 {
-  
-  if(Serial.available()>0)
+  if (Serial.available() > 0)
   {
-      delay(10);
-      dataSTB= Serial.readString();
-//      Serial.println(dataSTB);
-//      delay(000);
-      if(dataSTB=="o\n" ||dataSTB=="o" || dataSTB=="O\n" || dataSTB=="O")
+    delay(10);
+    dataSTB = Serial.readString();
+    //      Serial.println(dataSTB);
+    //      delay(000);
+    if (dataSTB == "o\n" || dataSTB == "o" || dataSTB == "O\n" || dataSTB == "O")
+    {
+      //        Serial.println("Diterima : open");
+      digitalWrite(pinRelaySolenoid, HIGH);
+      delay(2000);
+      digitalWrite(pinRelaySolenoid, LOW);
+      delay(100);
+      while (digitalRead(pinLimitSwitch) == LOW)
       {
-        Serial.println("Diterima : open");
-        digitalWrite(pinRelaySolenoid,HIGH);
-        delay(2000);
-        digitalWrite(pinRelaySolenoid,LOW);
         delay(100);
-        while(digitalRead(pinLimitSwitch)==LOW)
+        if (digitalRead(pinLimitSwitch) == HIGH)
         {
-          delay(100);
-          if(digitalRead(pinLimitSwitch)==HIGH)
-          {
-            Serial.println("closed");
-            break;
-          }
+          Serial.println("closed");
+          break;
         }
       }
-      
     }
-    else if(digitalRead(pinSensorNoTouch)== LOW)
-    {
-      Serial.println("exit");
-      digitalWrite(pinRelaySolenoid,HIGH);
-      delay(2000);
-      digitalWrite(pinRelaySolenoid,LOW);
-      delay(100);
-      while(digitalRead(pinLimitSwitch)==LOW)
-        {
-          delay(100);
 
-          if(digitalRead(pinLimitSwitch)==HIGH)
-          {
-            Serial.println("closed");
-            break;
-          }
-        }
+  }
+  else if (digitalRead(pinSensorNoTouch) == LOW)
+  {
+    Serial.println("exit");
+    digitalWrite(pinRelaySolenoid, HIGH);
+    delay(2000);
+    digitalWrite(pinRelaySolenoid, LOW);
+    delay(100);
+    while (digitalRead(pinLimitSwitch) == LOW)
+    {
+      delay(100);
+
+      if (digitalRead(pinLimitSwitch) == HIGH)
+      {
+        Serial.println("closed");
+        break;
+      }
     }
+  }
 }
